1d52b8e0a65a24a73600acc81f276223d636c0c7
wang <ishitora00@gmail.com>
2022-12-05 04:26:18 +0800

    fix:change baseurl

--- servers/API.ts
+++ servers/API.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL,
+  baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
   timeout: 10000,
 });
 
